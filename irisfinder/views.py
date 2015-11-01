05b50160d7f83a23746a55cfbd1d3dc9523d94d6
lauralorenz <lalorenz6@gmail.com>
2015-11-01 17:27:20 -0500

    Showing how to throw a scikit learn model right into the view logic and render its metadata in a template. We pull data from our iris dataset stored in our postgres database using the Django ORM.

--- irisfinder/views.py
+++ irisfinder/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 import datetime
 from models import Iris
+from sklearn import svm
+from sklearn.cross_validation import train_test_split
 
 # Create your views here.
 
@@ -11,5 +13,19 @@ def explore(request):
         "current_time": datetime.datetime.now().strftime('%I:%M:%S %p')
     }
     iris_data = Iris.objects.all()
-    data.update({"iris_data": iris_data})
+    X_fields = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width']
+    X_values = iris_data.values_list(*X_fields)
+    y_values = iris_data.values_list('species', flat=True)
+    data.update({"X": X_values, "Y": y_values})
+
+    X_train, X_test, y_train, y_test = train_test_split(X_values, y_values, test_size=.4)
+
+    clf = svm.SVC()
+    model = clf.fit(X_values, y_values)
+    score = clf.score(X_test, y_test)
+    data.update({"model": model, "score": score})
+
+    prediction = clf.predict([[5.3, 5.6, 8.9, 9.0]])
+    data.update({"prediction": prediction})
+
     return render(request, 'explore.html', context=data)
