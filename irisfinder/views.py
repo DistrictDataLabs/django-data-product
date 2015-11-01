347099962b611f51e325a81204002f1fd7d4ac50
lauralorenz <lalorenz6@gmail.com>
2015-11-01 15:45:28 -0500

    Setting up a common app-based URL heirarchy with the most simple view. URLROOT (set for us in settings.py) bounces URIs matching the regex r'^irisfinder/' to the url conf specified in irisfinder.urls, which in turn bounces further specified URIs matching the regex r'^explore' to the explore function in irisfinder.views, which simply returns some basic HTML wrapped in a django.http.HttpResponse object.

--- irisfinder/views.py
+++ irisfinder/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def explore(request):
+    return HttpResponse("<html><body>Hello World!</body></html>")
